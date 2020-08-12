ebc23aa600c6da2d00eea28d26108ed92736a3b3
Harman Sandhu <hrrsandhu6@gmail.com>
2020-08-11 22:38:37 -0700

    state bug

--- app/node.py
+++ app/node.py
@@ -4,17 +4,17 @@ import numpy as np
 
 STATE = {
     "undiscovered" : (50, 50, 50),
-    "discovered": (64, 118, 219),
+    "discovered": (0, 0, 255),
     "processed": (255, 0, 0)
 }
 class Node:
     def __init__(self, value, x, y, batch):
         self.value = value
-        self.circle = shapes.Circle(x, y, 30, batch=batch, color=STATE["discovered"])
+        self.circle = shapes.Circle(x, y, 30, batch=batch, color=STATE["undiscovered"])
         self.label = pyglet.text.Label(str(value), font_size=10, x=x, y=y, 
                                         anchor_x='center', anchor_y='center')             
         self.x = x
-        self.state = STATE["undiscovered"]
+        self.state = "undiscovered"
         self.y = y 
 
     def move(self, x, y):
