027a7893e3df2cbccd91152702c27430c8a04e08
Harman Sandhu <hrrsandhu6@gmail.com>
2020-08-11 22:37:21 -0700

    change and render state function - cState, and generate bfs_data

--- visgraph.py
+++ visgraph.py
@@ -9,15 +9,29 @@ from app.node import Node
 from app.line import Line
 from app.graph import Graph
 from pyglet import clock
-from time import sleep
 
-data = [3, 4, 1, 2, 5, 7, 9, 24, 25]
-edges = [(0,1), (2, 1), (2,0), (4, 3), (2, 5), (0, 4), (0, 6)]
+data = [3, 4, 1, 2, 5, 7, 9, 24, 25, 34, 26, 314, 343, 75, 78, 99, 10, 42, 11, 12, 432, 5352, 45]
+edges = [(0,1),
+         (2, 1), 
+         (2,0),
+         (4, 3), 
+         (2, 5), 
+         (0, 4), 
+         (0, 6), 
+         (4,5), 
+         (6,9), 
+         (2, 9), 
+         (2, 11), 
+         (1, 4),
+         (8, 9),
+         (10, 12),
+         (10, 0)
+        ]
 
 
 window = pyglet.window.Window(width=1200, height=800)
 batch = pyglet.graphics.Batch()
-# clock = pyglet.clock.Clock()
+
 
 graph = Graph(directed=False, data=data, edges=edges, batch=batch, window=window)
 
@@ -42,9 +56,14 @@ def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
             break
 
 
-
-def cState(t, node):
-    node.changeState("processed")
+def cState(t, bd):
+    for i in range(len(bd)):
+        if bd[i] == -1:
+            graph.NODES[i].changeState("undiscovered")
+        elif bd[i] == 0:
+            graph.NODES[i].changeState("discovered")
+        else:
+            graph.NODES[i].changeState("processed")
 
 
 @window.event
@@ -52,8 +71,10 @@ def on_key_press(symbol, modifiers):
     if symbol == key.Q:
         pyglet.app.exit()
     if symbol == key.SPACE:
-        for i in range(0, len(graph.NODES)):
-            clock.schedule_once(cState, i/4, graph.NODES[i])
+        graph.bfs(0)
+        bd = graph.bfs_data
+        for i in range(0, len(bd)):
+            clock.schedule_once(cState, i, bd[i])
     
 
 
