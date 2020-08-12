2aa1a9e90d4fa54c7e336a649cff3bd246663eec
Harman Sandhu <hrrsandhu6@gmail.com>
2020-08-11 22:38:11 -0700

    bfs and dfs

--- app/graph.py
+++ app/graph.py
@@ -2,9 +2,6 @@
 import numpy as np
 from .node import Node
 from .line import Line
-import time
-
-from pyglet import clock
 
 
 class Graph:
@@ -19,7 +16,9 @@ class Graph:
         self.edges = {}
         self.data = data
         self.i = 0
-
+        self.q = []
+        self.bfs_data = []
+        self.dfs_data = [] 
         for i in range(len(data)):
             rx = np.random.randint(200, window.width-200)
             ry = np.random.randint(100, window.height-100)
@@ -37,6 +36,44 @@ class Graph:
                 self.edges[self.NODES[e[0]]].append(self.NODES[e[1]])
                 self.edges[self.NODES[e[1]]].append(self.NODES[e[0]])
 
-
-    def bfs(self, index):
+    def adj(self, node_index):
         
+        enodes = self.edges[self.NODES[node_index]]
+        print(enodes)
+        indices = [self.NODES.index(i) for i in enodes]
+        return indices
+
+    def bfs(self, start):
+        nodes = [-1 for i in range(len(self.data))]
+
+        q = []
+        q.append(start)
+        nodes[start] = 0
+        self.bfs_data.append(nodes.copy())
+
+        while len(q) > 0:
+            v = q.pop(0)
+            nodes[v] = 0
+            self.bfs_data.append(nodes.copy())
+            for w in self.adj(v):
+                if nodes[w] == -1:
+                    q.append(w)
+            nodes[v] = 1
+            self.bfs_data.append(nodes.copy())
+
+    def dfs(self, start):
+        nodes = [-1 for i in range(len(self.data))]
+        s = []
+        s.insert(0,start)
+        self.dfs_data.append(nodes.copy())
+
+        while len(s) > 0:
+            v = s.pop(0)
+            
+            if nodes[v] == -1
+                nodes[v] = 0
+                self.dfs_data.append(nodes.copy())
+                for w in self.adj(v):
+                    s.insert(0, w)
+                nodes[v] = 1
+                self.dfs_data.append(nodes.copy())
